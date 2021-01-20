3a725337e4380272db454034af7d1fb788806785
Vitaliy5 <dydvitrom@ukr.net>
2021-01-21 01:35:18 +0200

    fix: item_id and auction_id in Auction and user_id in Bid are now required fields

--- src/main/java/com/interlink/auction/Models/Entities/Bid.java
+++ src/main/java/com/interlink/auction/Models/Entities/Bid.java
@@ -14,7 +14,7 @@ public class Bid {
     private double bid;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 
     @Column
