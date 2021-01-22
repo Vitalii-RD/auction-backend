d87c61ae4291b8e6a4fed9bfd744e7290ecce5ae
Vitaliy5 <dydvitrom@ukr.net>
2021-01-22 02:32:28 +0200

    add: method to get max bid record

--- src/main/java/com/interlink/auction/Repositories/BidRepository.java
+++ src/main/java/com/interlink/auction/Repositories/BidRepository.java
@@ -9,4 +9,8 @@ import java.util.List;
 public interface BidRepository extends JpaRepository<Bid, Long> {
     @Query(value = "SELECT * FROM bids WHERE bids.auction_id = ?1 ORDER BY bids.bid", nativeQuery = true)
     List<Bid> findAllByAuctionId(long id);
+
+    @Query(value = "SELECT * FROM (SELECT * FROM bids WHERE auction_id = ?1) AS auc " +
+            "WHERE bid IN (SELECT MAX(bid) FROM bids WHERE auction_id = ?1);", nativeQuery = true)
+    Bid findMaxBidInAuction(Long auctionId);
 }
