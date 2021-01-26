f2589c1617865bdce9661a24e310e147ced57749
Vitaliy5 <dydvitrom@ukr.net>
2021-01-26 17:07:37 +0200

    add InvalidBidValueException

---
+++ src/main/java/com/interlink/auction/Exceptions/InvalidBidValueException.java
@@ -0,0 +1,7 @@
+package com.interlink.auction.Exceptions;
+
+public class InvalidBidValueException extends Exception{
+    public InvalidBidValueException(String message) {
+        super(message);
+    }
+}
